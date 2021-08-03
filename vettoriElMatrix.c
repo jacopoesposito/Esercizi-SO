94a83d6dfd780a92d9fa209986f73b90bf311ce2
jacopoesposito <jaco.espo@hotmail.it>
2021-08-03 21:23:04 +0200

    Effettuati alcuni FIX e aggiunti alcuni commenti

--- vettoriElMatrix.c
+++ vettoriElMatrix.c
@@ -5,7 +5,7 @@ provveda poi a lanciare n thread (uno per riga). Ciascun thread legge
 un valore casuale sulla propria riga e lo inserisce, a turno (un
 inserimento al secondo), in un vettore di n elementi. 
 Un ulteriore thread attende il riempimento del vettore, lo stampa, e termina.
-Si usino semafori Posix con nome e variabili di condizione. */
+Si usino semafori Posix per memoria e variabili di condizione. */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,7 +20,6 @@ int **matrix;
 pthread_t *lettori;
 pthread_t master;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutexCond = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
 sem_t contatore;
 int *array;
@@ -32,6 +31,7 @@ void *leggiScrivi(void *arg);
 void *stmpResult(void *arg);
 
 int main(int argc, char **argv){
+    srand(time(NULL));
 
     if(argc > 1){
         n = atoi(argv[1]);
@@ -73,7 +73,6 @@ int main(int argc, char **argv){
 
     sem_destroy(&contatore);
     pthread_mutex_destroy(&mutex);
-    pthread_mutex_destroy(&mutexCond);
     pthread_cond_destroy(&condition);
 
     return 0;    
@@ -89,26 +88,23 @@ void *leggiScrivi(void *arg){
         indice++;
         printf("Indice ->%d\n", indice);
         sleep(1);
-    pthread_mutex_unlock(&mutex);
-
-    pthread_mutex_lock(&mutexCond);
         if(indice == n){
-            pthread_cond_signal(&condition);
+            pthread_cond_signal(&condition); //Se il numero dell'indice è uguale ad N allora invio il segnale al thread Master
         }
-    pthread_mutex_unlock(&mutexCond);
+    pthread_mutex_unlock(&mutex);
     
     pthread_exit(NULL);
 }
 
 void *stmpResult(void *arg){
-    pthread_mutex_lock(&mutexCond);
+    pthread_mutex_lock(&mutex);
         while(indice < n)
-            pthread_cond_wait(&condition, &mutexCond);
-            for(int i = 0; i < n; i++){
-                sem_post(&contatore);
-                printf("Elemento %d del vettore = %d\n",i, array[i]);
-            }
-    pthread_mutex_unlock(&mutexCond);
+            pthread_cond_wait(&condition, &mutex); //Aspetto fintanto che la condizione non si è realizzata
+        for(int i = 0; i < n; i++){
+            sem_post(&contatore);
+            printf("Elemento %d del vettore = %d\n",i, array[i]);
+        }
+    pthread_mutex_unlock(&mutex);
 }
 
 void riempiMatrici(int **matrix){
